2f1de7043fd1a4422312997d7010227ac3c8782e
Breno Araripe <brenoma@live.com>
2023-12-12 09:39:15 -0300

    start project

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Breno Araripe on 11/12/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 100)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds remaining").font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityLabel("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    MeetingView()
+}
